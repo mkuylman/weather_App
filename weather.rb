0e017bc6d0bf8a1b2bd1a237406ab06d210c1284
mkuylman <mkuylman@gmail.com>
2016-01-10 16:27:45 -0800

    Initial Commit

---
+++ weather.rb
@@ -0,0 +1,73 @@
+require 'sinatra'
+require 'yahoo_weatherman'
+
+def get_weather(location)
+  client = Weatherman::Client.new
+  client.lookup_by_location(location).condition['text']
+end
+
+def get_temp(location)
+  client = Weatherman::Client.new 
+  response = client.lookup_by_location(location).condition['temp']
+end
+
+get '/' do 
+  erb :home
+end
+
+post '/weather' do
+  @post = params[:post]['location']
+  @weather = get_weather(@post).downcase
+  @temp = ((get_temp(@post)* 9) / 5) + 32
+  
+  "#{@weather}"
+  
+  if (@weather == 'sunny')
+    erb :sunny
+  elsif (@weather == 'fair')
+    erb :sunny
+  elsif (@weather == 'cloudy')
+    erb :cloudy
+  elsif (@weather == 'mostly cloudy')
+    erb :cloudy
+  elsif (@weather == 'partly cloudy')
+    erb :cloudy
+  elsif (@weather ==  'fog')
+    erb :cloudy
+  elsif (@weather == 'snow')
+    erb :snowy
+  elsif (@weather == 'rain')
+    erb :rainy
+  elsif (@weather == 'heavy rain')
+    erb :rainy
+  else 
+    erb :default
+  end  
+end
+
+    
+    
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
